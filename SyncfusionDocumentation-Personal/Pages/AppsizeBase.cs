50d659ba0776d9e94a2e61b1c0a1e0b2265bb8da
Gilly <MagnusMq07@gmail.com>
2023-11-15 15:34:05 +0300

    Let's package this

---
+++ SyncfusionDocumentation-Personal/Pages/AppsizeBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+
+namespace SyncfusionDocumentation_Personal.Pages
+{
+    public class AppsizeBase : ComponentBase
+    {
+        [Inject]
+        public IJSRuntime jSRuntime { get; set; }
+
+
+        private async void callOntouch(MouseEventArgs args)
+        {
+            await jSRuntime.InvokeAsync<string>("onTouch");
+        }
+    }
+}
