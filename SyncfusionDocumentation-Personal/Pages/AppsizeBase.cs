5f7f492439f1cc561f6c444a87e21a625041a745
Gilly <MagnusMq07@gmail.com>
2023-11-16 15:32:44 +0300

    Let's build and learn slowly but surely

--- SyncfusionDocumentation-Personal/Pages/AppsizeBase.cs
+++ SyncfusionDocumentation-Personal/Pages/AppsizeBase.cs
@@ -9,10 +9,16 @@ namespace SyncfusionDocumentation_Personal.Pages
         [Inject]
         public IJSRuntime jSRuntime { get; set; }
 
+        public DateTime? DateValue { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 24);
 
-        private async void callOntouch(MouseEventArgs args)
+
+        private async void callOnTouch(MouseEventArgs args)
         {
             await jSRuntime.InvokeAsync<string>("onTouch");
+        }
+        private async void callOnMouse(MouseEventArgs args)
+        {
+            await jSRuntime.InvokeAsync<string>("onMouse");
         }
     }
 }
