f8a573622ba4f841295137338b96796d4b3cf5fd
Gilly <MagnusMq07@gmail.com>
2023-12-07 15:38:55 +0300

    last

--- SyncfusionDocumentation-Personal/DataBinding/TwoWayDatabindingBase.cs
+++ SyncfusionDocumentation-Personal/DataBinding/TwoWayDatabindingBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Calendars;
 
 namespace SyncfusionDocumentation_Personal.DataBinding
 {
@@ -15,5 +16,31 @@ namespace SyncfusionDocumentation_Personal.DataBinding
         {
             { "bind", "textValue" }
         };
+
+        //Component rendering code
+        public DateTime? DateValue { get; set; } = DateTime.Now.Date;
+        protected RenderFragment dynamicComponent { get; set; }
+        protected RenderFragment CreateComponent() => builder =>
+        {
+            builder.OpenComponent(0, typeof(SfDatePicker<DateTime>));
+            builder.AddAttribute(1, "ID", "MyDynamicId");
+            builder.AddAttribute(2, "Placeholder", "Choose a date");
+            //Binding the value property with DateValue property.
+            builder.AddAttribute(3, "Value", DateValue);
+            builder.AddAttribute(4, "onchange", Microsoft.AspNetCore.Components.EventCallback.Factory.
+                    CreateBinder(this, _value => DateValue = _value, DateValue));
+
+            builder.CloseComponent();
+        };
+
+        protected void RenderComponent()
+        {
+            dynamicComponent = CreateComponent();
+        }
+        protected void onChange()
+        {
+            DateValue = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 07);
+        }
+
     }
 }
