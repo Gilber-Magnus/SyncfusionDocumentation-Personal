69341eba5dd4060fab2681aeb39d80094c0091a0
Magnus Mutai <MagnusMq07@gmail.com>
2024-01-04 11:46:37 +0300

    Starting Static localization for server afresh

--- SyncfusionDocumentation-Personal/Program.cs
+++ SyncfusionDocumentation-Personal/Program.cs
@@ -4,6 +4,8 @@ using SyncfusionDocumentation_Personal.Data;
 using Syncfusion.Blazor;
 using Microsoft.EntityFrameworkCore;
 using SyncfusionDocumentation_Personal;
+using Microsoft.JSInterop;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,13 +18,34 @@ builder.Services.AddDbContext<OrderDetailsDbContext>(option =>
                 option.UseSqlServer(builder.Configuration.GetConnectionString("OrdersDetailsDatabase")));
 //custom adaptor as component service
 builder.Services.AddScoped<Order>();
+//Register the Syncfusion locale service to localize Syncfusion Blazor components.
 builder.Services.AddSingleton(typeof(ISyncfusionStringLocalizer), typeof(SyncfusionLocalizer));
+
+var host = builder.Build();
+
+//Setting culture of the application
+var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+var result = await jsInterop.InvokeAsync<string>("cultureInfo.get");
+CultureInfo culture;
+if (result != null)
+{
+    culture = new CultureInfo(result);
+}
+else
+{
+    culture = new CultureInfo("en-US");
+    await jsInterop.InvokeVoidAsync("cultureInfo.set", "en-US");
+}
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+//End of culture
+
 var app = builder.Build();
 
 //Register Syncfusion License
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEZiWH1dcHVWTmNaVkFzWw==");
 
-//app.UseRequestLocalization("de-DE");
+app.UseRequestLocalization("de-DE");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
